063a4c77f84f63ec5a9bc216d3e14027cdbc189c
Georges JK Kim <kimju0913@naver.com>
2020-07-02 22:39:10 +0900

    test

---
+++ Sort/src/sort/DefInsertionSort.java
@@ -0,0 +1,44 @@
+package sort;
+
+public class DefInsertionSort {
+	
+	public static void main(String[] args) {
+	
+		// 삽입 정렬(Insertion Sort)
+		// - 각 숫자를 적절한 위치에 삽입하는 방식으로 문제를 해결
+		// - 삽입 정렬은 필요할 때만 위치를 변경
+		// - 하지만 시간복잡도는 역시 O(N^2), 그래도 나머지 애들(버블, 선택) 보다는 효율적 
+		
+		
+		
+		//--------------------------------------------------------------------------------------	
+		
+		// 다음의 숫자들을 오름차순으로 정렬하는 프로그램을 작성하시오
+		// 1 10 3 6 2 4 9 8 5 7
+		
+		int arr [] = {1, 10, 3, 6, 2, 4, 9, 8, 5, 7};
+		int temp = 0;
+		boolean check = false;
+		
+		1 3 6 10 2 5 4
+		for(int i = 1; i < arr.length; i++) {
+			int k = 0;
+			while(check == true){
+				
+				
+			}
+			
+		
+		}// end outer for() 
+		
+		
+		
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+		
+	}// end main()
+	
+	
+}

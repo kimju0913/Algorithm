a4c5b7f77079c72e662c3898ae430ad18f61b583
Georges JK Kim <kimju0913@naver.com>
2020-07-13 17:35:58 +0900

    update

--- Search/src/search/DefBreathFirstSearch.java
+++ Search/src/search/DefBreathFirstSearch.java
@@ -29,7 +29,7 @@ public class DefBreathFirstSearch {
 			{0, 0, 1, 0, 0, 0},
 			{0, 0, 0, 0, 0, 0},
 			{0, 0, 1, 0, 0, 0}			
-};;
+	};;
 	static boolean visit[];
 	
 	public static void main(String[] args) {

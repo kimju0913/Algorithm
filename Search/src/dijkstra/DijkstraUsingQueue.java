0c49a0ce1b246f8c14efe9054c57321ee3fc3c51
Georges JK Kim <kimju0913@naver.com>
2020-08-18 14:11:03 +0900

    update

--- Search/src/dijkstra/DijkstraUsingQueue.java
+++ Search/src/dijkstra/DijkstraUsingQueue.java
@@ -1,5 +1,7 @@
 package dijkstra;
 
+import java.util.PriorityQueue;
+
 public class DijkstraUsingQueue {
 
 	// ** 우선순위 큐를 활용한 다익스트라 알고리즘
@@ -7,6 +9,44 @@ public class DijkstraUsingQueue {
 	// - 기존 다익스트라의 경우 시간복잡도가 O(N^2)임.
 	//   이 경우 노드가 많고, 간선이 적을 때 엄청나게 비효율적인 코드가 됨 
 	// - https://doublezerostone.tistory.com/31 
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+	
+	public void dikstraQueue(Graph g) {
+		PriorityQueue<Element> queue = new PriorityQueue<Element>();
+		// https://manducku.tistory.com/32
+		// TODO : 위 페이지 보고 다익스트라 큐 버전 만들기 
+		// 		  그래프 클라스를 받아와서 그 클라스에 getN() 함수 만들어서 n값 받아오기.
+		
+		
+		
+	}
+	
+	
+	
+	
+}
+
+class Element implements Comparable<Element>{
+
+	int vertex;
+	int distance;
+	
+	public Element(int vertex, int distance) {
+		this.vertex = vertex;
+		this.distance = distance;
+	}
+	
+	
+	@Override
+	public int compareTo(Element o) {		
+		return distance <= o.distance ? -1 : 1;
+	}
+	
 	
 	
 }
